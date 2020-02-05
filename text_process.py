14274c174188bc45818c2f1f50a19e4c1e24dfca
masuda wataru <wataru-masuda@GD0033.local>
2020-02-05 11:14:48 +0900

    スクレイぷ処理、形態素解析は共通化して抜き出す

---
+++ text_process.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import sys
+
+from gensim.corpora.dictionary import Dictionary
+import requests
+from bs4 import BeautifulSoup
+import MeCab
+
+
+def fetch_contents_from_url(url, encoding="UTF-8", use_domain=False, timeout=3.0):
+    try:
+        res = requests.get("http://"+url, timeout=timeout) if use_domain else requests.get(url, timeout=timeout)
+    except Exception:
+        print("GET request failed")
+        return None
+
+    res.encoding = encoding
+    try:
+        soup = BeautifulSoup(res.text, 'html.parser')
+    except Exception:
+        print("failed to parse html text")
+        return None
+        
+    for meta_tag in soup.find_all('meta', attrs={'name': 'description'}):
+        if soup.title:
+            return str(soup.title.string) + "\n" + str(meta_tag.get('content'))
+        else:
+            return None
+        
+def doc2word_list(text):
+    m = MeCab.Tagger("-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
+    word_list = []
+    for row in m.parse(text).split("\n"):
+        word = row.split()[0]
+        if word == "EOS":
+            break
+        pos = row.split()[-1]
+        if "名詞-サ変接続" in pos or "数" in pos:
+            continue
+        if "名詞" in pos or "形容詞" in pos or "動詞" in pos:
+            word_list.append(word)
+    return word_list
