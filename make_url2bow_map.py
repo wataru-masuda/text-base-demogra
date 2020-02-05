1c503caaa92641bbfebc241495f4b3f69efefe03
masuda wataru <wataru-masuda@GD0033.local>
2020-02-05 12:00:06 +0900

    コード整理

--- make_url2bow_map.py
+++ make_url2bow_map.py
@@ -3,52 +3,9 @@ import argparse
 import sys
 
 from gensim.corpora.dictionary import Dictionary
-import requests
-from bs4 import BeautifulSoup
-import MeCab
-import matplotlib.pyplot as plt
-from collections import Counter
 
+from text_process import fetch_contents_from_url, doc2word_list
 
-def fetch_title_and_description(url, encoding="UTF-8", use_domain=False, timeout=3.0):
-    try:
-        res = requests.get("http://"+url, timeout=timeout) if use_domain else requests.get(url, timeout=timeout)
-    except Exception:
-        print("GET request failed")
-        return None
-
-    res.encoding = encoding
-    try:
-        soup = BeautifulSoup(res.text, 'html.parser')
-    except Exception:
-        print("failed to parse html text")
-        return None
-        
-    for meta_tag in soup.find_all('meta', attrs={'name': 'description'}):
-        if soup.title:
-            return str(soup.title.string) + "\n" + meta_tag.get('content')
-        else:
-            return None
-        
-def doc2word_list(text):
-    m = MeCab.Tagger("-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
-    word_list = []
-    for row in m.parse(text).split("\n"):
-        word = row.split()[0]
-        if word == "EOS":
-            break
-        pos = row.split()[-1]
-        if "名詞-サ変接続" in pos or "数" in pos:
-            continue
-        if "名詞" in pos or "形容詞" in pos or "動詞" in pos:
-            word_list.append(word)
-    return word_list
-
-
-def save_word_cnt():
-    with open("word_cnt.txt","w") as wf:
-        for word, cnt in word_cnt.items():
-            wf.write("{}\t{}\n".format(word, cnt))
             
 def main():
     parser = argparse.ArgumentParser()
@@ -61,7 +18,7 @@ def main():
     f = open("url2bow_map.csv", "a")
     for i, url in enumerate(sys.stdin):
         print("url " + str(i))
-        text = fetch_title_and_description(url.strip(), use_domain=args.use_domain)
+        text = fetch_contents_from_url(url.strip(), use_domain=args.use_domain)
         if not text:
             continue
         
