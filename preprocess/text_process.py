15d17ee81a81d9ac12253bb03c1748d6bf45bfc3
masuda wataru <wataru-masuda@GD0033.local>
2020-02-26 11:53:36 +0900

    stopwords読む＆1文字は飛ばす

--- preprocess/text_process.py
+++ preprocess/text_process.py
@@ -29,6 +29,9 @@ def fetch_contents_from_url(url, encoding="UTF-8", use_domain=False, timeout=3.0
         
 def doc2word_list(text):
     m = MeCab.Tagger("-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
+
+    with open("/Users/wataru-masuda/Documents/src/github/text-base-demogra/preprocess/stopwords.txt", "r") as f:
+        stopwords = set(w.strip() for w in f.readlines())
     word_list = []
     for row in m.parse(text).split("\n"):
         word = row.split()[0]
@@ -36,6 +39,10 @@ def doc2word_list(text):
             break
         pos = row.split()[-1]
         if "名詞-サ変接続" in pos or "数" in pos:
+            continue
+        if len(word) == 1:
+            continue
+        if word in stopwords:
             continue
         if "名詞" in pos or "形容詞" in pos or "動詞" in pos:
             word_list.append(word)
